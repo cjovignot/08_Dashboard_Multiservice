7cb79100b0f3510886621fc2f0a1bf1efe416971
dinoBALLETTI <db00291@hotmail.com>
2023-05-02 17:26:08 +0200

    [ADD] base GET request in back

--- speedrun/speedrun.js
+++ speedrun/speedrun.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3003;
+const port = process.env.SPEEDRUN_PORT;
+const url = process.env.SPEEDRUN_API_URL;
+const axios = require('axios');
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('Hello SpeedRun coucou lorie et cosme!'));
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+        res.json(data);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({error: "ERROR !!"});
+    }
+});
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
