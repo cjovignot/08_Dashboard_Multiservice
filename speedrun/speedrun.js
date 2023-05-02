133221aa046bf40856dc959d411b18b154eb67ec
dinoBALLETTI <db00291@hotmail.com>
2023-05-02 15:57:13 +0200

    [ADD] fresh repo

--- speedrun/speedrun.js
+++ speedrun/speedrun.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3003;
 
-app.get('/', (req, res) => res.send('Hello SpeedRun!'));
+app.get('/', (req, res) => res.send('Hello SpeedRun coucou lorie et cosme!'));
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
