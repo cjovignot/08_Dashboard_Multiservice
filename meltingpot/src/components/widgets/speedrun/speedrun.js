9673edc7939c1ed4773b840915e943bba21f4903
dinoBALLETTI <db00291@hotmail.com>
2023-05-04 17:09:38 +0200

    [ADD] rien

---
+++ meltingpot/src/components/widgets/speedrun/speedrun.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const speedrun = () => {
+
+    const [gameTitle, setGameTitle] = useState('');
+
+    const handleChange = async (e) => {
+        e.preventDefault();
+        console.log(gameTitle);
+
+        try {
+            const gamesByTitle = await axios.get(`http://localhost:3003?name=${gameTitle}`);
+            console.log(gamesByTitle.data.data);
+            response = gamesByTitle.data.data;
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <div className="card w-128 bg-base-100 shadow-xl image-full m-auto m-10">
+
+            <div className="card-body">
+                <h2 className="card-title">⚡ speedrun.com</h2>
+
+                <form onSubmit={handleChange}>
+                    <input
+                        type="text"
+                        placeholder="Which game ?"
+                        className="input w-full max-w-xs"
+                        onChange= {(e) => setGameTitle(e.target.value)}
+                    ></input>
+                    <button type="submit">Search</button>
+                    
+                    <label for="game-select">Choose a game:</label>
+                    <select name="games" id="id">
+                        <option value="">--Please choose a version--</option>
+                        <option value="name">Le nom de mon jeu</option>
+                    </select>
+                </form>
+
+                <div className="gameData_ctnr">
+                    <div className="gamePicture_ctnr">pic</div>
+                    <div className="gameTitle_ctnr">title</div>
+                    <div className="btn btn-circle gameFavBtn_ctnr">❤️</div>
+                </div>
+
+                <div className="top_ctnr">
+                    <div className="top1">🥇 Runner name</div>
+                    <div className="top1">🥈 Runner name</div>
+                    <div className="top1">🥉 Runner name</div>
+
+                </div>
+
+                <div className="fav_ctnr flex-row justify-evenly">
+                    <div className="btn favA">game title 1</div>
+                    <div className="btn favB">game title 2</div>
+                    <div className="btn favC">game title 3</div>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default speedrun;
--- meltingpot/src/pages/api/speedrun_api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://www.speedrun.com/api/v1/games",
-});
