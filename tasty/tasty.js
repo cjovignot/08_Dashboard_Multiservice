d063c14097353c4964b9d9b22a5691767e5303b0
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-05-05 17:43:32 +0200

    [SAVE] 01

--- tasty/tasty.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3007;
-
-app.get('/', (req, res) => res.send('Hello Tasty'));
-
-app.listen(port, () => console.log(`Express app running on port ${port}!`));
