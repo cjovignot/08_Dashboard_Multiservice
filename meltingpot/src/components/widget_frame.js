7f16cb36d4788ac006cbb7401c987f2deebec176
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-05-06 11:30:46 +0200

    [WIP] Grid adding Food & Emoji

--- meltingpot/src/components/widget_frame.js
+++ meltingpot/src/components/widget_frame.js
@@ -4,8 +4,8 @@ import Pokemon from "../components/widgets/pokemon";
 import Postit from "../components/widgets/postIt";
 import SpeedRun from "../components/widgets/speedrun";
 import Spotify from "../components/widgets/spotify";
-import Tasty from "../components/widgets/tasty";
-import Trump from "../components/widgets/trump";
+// import Food from "./widgets/food";
+// import Emoji from "../components/widgets/emoji";
 import Valorant from "../components/widgets/valorant";
 import Weather from "../components/widgets/weather";
 import Zelda from "../components/widgets/zelda";
@@ -21,11 +21,11 @@ const Frame = ({ isLogged }) => {
     { i: "spotify", x: 2, y: 0, w: 1, h: 2 },
     { i: "pokemon", x: 0, y: 1, w: 1, h: 2 },
     { i: "postit", x: 1, y: 1, w: 1, h: 2 },
-    { i: "tasty", x: 2, y: 1, w: 1, h: 2 },
-    { i: "trump", x: 0, y: 2, w: 1, h: 2 },
+    { i: "food", x: 2, y: 1, w: 1, h: 2 },
+    { i: "emoji", x: 0, y: 2, w: 1, h: 2 },
     { i: "valorant", x: 1, y: 2, w: 1, h: 2 },
     { i: "speedrun", x: 2, y: 2, w: 1, h: 2 },
-  ];
+    ];
 
 
   return (
@@ -52,12 +52,12 @@ const Frame = ({ isLogged }) => {
         <div key="spotify">
           <Spotify isLogged={isLogged} />
         </div>
-        <div key="tasty">
-          <Tasty />
-        </div>
-        <div key="trump">
-          <Trump />
-        </div>
+        {/* <div key="food">
+          <Food />
+        </div> */}
+        {/* <div key="emoji">
+          <Emoji />
+        </div> */}
         <div key="valorant">
           <Valorant />
         </div>
