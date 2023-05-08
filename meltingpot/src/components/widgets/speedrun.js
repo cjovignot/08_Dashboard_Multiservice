87f6e90a494b778617bbdd2ec15c1ace77214707
dinoBALLETTI <db00291@hotmail.com>
2023-05-08 14:08:27 +0200

    [ADD]front OK]

--- meltingpot/src/components/widgets/speedrun.js
+++ meltingpot/src/components/widgets/speedrun.js
@@ -2,23 +2,54 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const speedrun = () => {
-
-    const [gameTitle, setGameTitle] = useState('');
-  
+    // Game Data
+    const [gameSearch, setGameSearch] = useState('');
+    const [gameCover, setGameCover] = useState('');
+    const [gameName, setGameName] = useState('');
+    const [catName, setCatName] = useState('');
+    // 1st Place Data
+    const [runnerNameGold, setRunnerNameGold] = useState('');
+    const [runnerTimeGold, setRunnerTimeGold] = useState('');
+    const timeGoldMinutes = Math.floor(runnerTimeGold/60);
+    const timeGoldSeconds = (runnerTimeGold - timeGoldMinutes * 60);
+    // 2nd Place Data
+    const [runnerNameSilver, setRunnerNameSilver] = useState('');
+    const [runnerTimeSilver, setRunnerTimeSilver] = useState('');
+    const timeSilverMinutes = Math.floor(runnerTimeSilver/60);
+    const timeSilverSeconds = (runnerTimeSilver - timeSilverMinutes * 60);
+    // 3rd Place Data
+    const [runnerNameBronze, setRunnerNameBronze] = useState('');
+    const [runnerTimeBronze, setRunnerTimeBronze] = useState('');
+    const timeBronzeMinutes = Math.floor(runnerTimeBronze/60);
+    const timeBronzeSeconds = (runnerTimeBronze - timeBronzeMinutes * 60);
 
     const handleChange = async (e) => {
         e.preventDefault();
-        console.log(gameTitle);
+        console.log(gameSearch);
 
         try {
-            const gamesByTitle = await axios.get(`http://localhost:3003?name=${gameTitle}`);
-            console.log(gamesByTitle.data);
-            response = gamesByTitle;
+            const response = await axios.get(`http://localhost:3003?name=${gameSearch}`);
+            setGameCover(response.data.GAMECOVER);
+            setGameName(response.data.GAMENAME);
+            setCatName(response.data.CATNAME);
+            setRunnerNameGold(response.data["1-RUNNER_GOLD"].RUNNERNAME_GOLD);
+            setRunnerTimeGold(response.data["1-RUNNER_GOLD"].RUNNERTIME_GOLD);
+            setRunnerNameSilver(response.data["2-RUNNER_SILVER"].RUNNERNAME_SILVER);
+            setRunnerTimeSilver(response.data["2-RUNNER_SILVER"].RUNNERTIME_SILVER);
+            setRunnerNameBronze(response.data["3-RUNNER_BRONZE"].RUNNERNAME_BRONZE);
+            setRunnerTimeBronze(response.data["3-RUNNER_BRONZE"].RUNNERTIME_BRONZE);
 
         } catch (error) {
             console.error(error);
         }
     }
+
+    function toTimeString(totalSeconds) {
+        const totalMs = totalSeconds * 1000;
+        const result = new Date(totalMs).toISOString().slice(11, 19);
+      
+        return result;
+      }
 
     return (
         <div className="card w-128 bg-base-100 shadow-xl image-full m-auto m-10">
@@ -31,29 +62,25 @@ const speedrun = () => {
                         type="text"
                         placeholder="Which game ?"
                         className="input w-full max-w-xs"
-                        onChange= {(e) => setGameTitle(e.target.value)}
+                        onChange= {(e) => setGameSearch(e.target.value)}
                     ></input>                    
                 </form>
 
-                <div className="gameData_ctnr">
-                    <div className="gamePicture_ctnr">pic</div>
-                    <div className="gameTitle_ctnr">title</div>
-                    {/* <div className="btn btn-circle gameFavBtn_ctnr">❤️</div> */}
+                <div className="gameData_ctnr flex-column items-center">
+                    <div className="gameCover_ctnr"><img src={gameCover}></img></div>
+                    <div className="gameTitle_ctnr">{gameName} - {catName}</div>
                 </div>
 
-                <div className="top_ctnr">
-                    <div className="top1">🥇 Runner name</div>
-                    <div className="top1">🥈 Runner name</div>
-                    <div className="top1">🥉 Runner name</div>
+                <div className="top_ctnr justify-center">
 
-                </div>
+                    {/* <div className="top1">🥇 {runnerNameGold}  🕒 {timeGoldMinutes}m {timeGoldSeconds}s</div>
+                    <div className="top1">🥈 {runnerNameSilver}  🕒 {timeSilverMinutes}m {timeSilverSeconds}s</div>
+                    <div className="top1">🥉 {runnerNameBronze}  🕒 {timeBronzeMinutes}m {timeBronzeSeconds}s</div> */}
+                    <div className="top1">🥇 {runnerNameGold}  🕒 {toTimeString(runnerTimeGold)}</div>
+                    <div className="top1">🥈 {runnerNameSilver}  🕒 {toTimeString(runnerTimeSilver)}</div>
+                    <div className="top1">🥉 {runnerNameBronze}  🕒 {toTimeString(runnerTimeBronze)}</div>
 
-                {/* <div className="fav_ctnr flex-row justify-evenly">
-                    <div className="btn favA">game title 1</div>
-                    <div className="btn favB">game title 2</div>
-                    <div className="btn favC">game title 3</div>
-                </div> */}
-                
+                </div>                
             </div>
         </div>
     );
